ce64d7d9baa5ad83bb8e10b4409b8753a7c0fef5
Ilya Shakhat <shakhat@gmail.com>
2017-06-08 16:53:56 +0200

    Cleanup code of DevStack plugin
    Change-Id: I798e56d5779705ce0a5ec7889af688fac8016d5e

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,4 +1,4 @@
-# DevStack extras script to install Rally
+# DevStack extras script to install osprofiler
 
 # Save trace setting
 XTRACE=$(set +o | grep xtrace)
@@ -6,18 +6,9 @@ set -o xtrace
 
 source $DEST/osprofiler/devstack/lib/osprofiler
 
-if [[ "$1" == "source" ]]; then
-    # Initial source
-    source $TOP_DIR/lib/rally
-# elif [[ "$1" == "stack" && "$2" == "install" ]]; then
-#    echo_summary "Installing OSprofiler"
-#    install_rally
-elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+if [[ "$1" == "stack" && "$2" == "post-config" ]]; then
     echo_summary "Configuring OSprofiler"
     configure_osprofiler
-elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
-    echo_summary "Initializing OSprofiler"
-    init_osprofiler
 fi
 
 # Restore xtrace
